8e6bb0b823f4e4bd34c4267664035e0178729edc
serIvanich <serivan1108@gmail.com>
2021-05-11 21:56:39 +0300

    Updates

---
+++ precache-manifest.bd44d86f9d83252ac63567b9ef8dc05e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1bc115a869903f8e1fc3d018e9669d",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d60c35149238d0d06440",
+    "url": "/todo-list/static/css/main.e7b63e06.chunk.css"
+  },
+  {
+    "revision": "71ab7e11e3b4277e45cf",
+    "url": "/todo-list/static/js/2.48c94700.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todo-list/static/js/2.48c94700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60c35149238d0d06440",
+    "url": "/todo-list/static/js/main.cd758040.chunk.js"
+  },
+  {
+    "revision": "f5ee74d33c5afdf3bdbc",
+    "url": "/todo-list/static/js/runtime-main.bd857350.js"
+  }
+]);
