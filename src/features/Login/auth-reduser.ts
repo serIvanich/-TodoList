a7b1e4670146c994f8c6846e6291120b3e4d984d
serIvanich <serivan1108@gmail.com>
2021-09-18 19:39:12 +0300

    auth-toolkit

--- src/features/Login/auth-reduser.ts
+++ src/features/Login/auth-reduser.ts
@@ -1,13 +1,12 @@
-import {Dispatch} from 'redux'
 import {setAppStatusAC} from '../../app/app-reducer'
 import {authApi, FieldErrorType, LoginRequestType} from "../../api/todolist-api";
 import {handleServerAppError, handleServerNetworkError} from "../../utils/error-utils";
 import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
-import { AxiosError } from 'axios';
+import {AxiosError} from 'axios';
 
-export const loginTC = createAsyncThunk<{isLoggedIn: boolean},LoginRequestType,
-    {rejectValue:{errors: Array<string>, fieldsErrors?: Array<FieldErrorType>}} >
-('auth/loading', async (param, thunkAPI) => {
+export const loginTC = createAsyncThunk<undefined, LoginRequestType,
+    { rejectValue: { errors: Array<string>, fieldsErrors?: Array<FieldErrorType> } }>
+('auth/login', async (param, thunkAPI) => {
     thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
     try {
 
@@ -15,18 +14,57 @@ export const loginTC = createAsyncThunk<{isLoggedIn: boolean},LoginRequestType,
         if (data.resultCode === 0) {
 
             thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
-           return {isLoggedIn: true}
+            return
         } else {
 
             handleServerAppError(data, thunkAPI.dispatch)
             return thunkAPI.rejectWithValue({errors: data.messages, fieldsErrors: data.fieldsErrors})
         }
     } catch (e) {
-const error: AxiosError = e
+        const error: AxiosError = e
         handleServerNetworkError(error.message, thunkAPI.dispatch)
         return thunkAPI.rejectWithValue({errors: [error.message], fieldsErrors: undefined})
     }
 })
+
+
+export const logoutTC = createAsyncThunk
+('auth/logout', async (param, thunkAPI) => {
+    try {
+        thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
+        const data = await authApi.logout()
+        if (data.resultCode === 0) {
+            thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
+            return
+        } else {
+
+            handleServerAppError(data, thunkAPI.dispatch)
+            return thunkAPI.rejectWithValue({})
+        }
+    } catch (e) {
+
+        handleServerNetworkError(e.message, thunkAPI.dispatch)
+        return thunkAPI.rejectWithValue({})
+    }
+})
+
+// export const logoutTC = () => async (dispatch: Dispatch) => {
+//     try {
+//         dispatch(setAppStatusAC({status: 'loading'}))
+//         const data = await authApi.logout()
+//         if (data.resultCode === 0) {
+//
+//             dispatch(setIsLoggedInAC({value: false}))
+//             dispatch(setAppStatusAC({status: 'succeeded'}))
+//         } else {
+//
+//             handleServerAppError(data, dispatch)
+//         }
+//     } catch (e) {
+//
+//         handleServerNetworkError(e.message, dispatch)
+//     }
+// }
 
 // export const loginTC_ = (payload: LoginRequestType) => async (dispatch: Dispatch) => {
 //     try {
@@ -58,8 +96,11 @@ const slice = createSlice({
         }
     },
     extraReducers: (builder) => {
-        builder.addCase(loginTC.fulfilled,(state, action) => {
-            state.isLoggedIn = action.payload.isLoggedIn
+        builder.addCase(loginTC.fulfilled, (state) => {
+            state.isLoggedIn = true
+        })
+        builder.addCase(logoutTC.fulfilled, (state) => {
+            state.isLoggedIn = false
         })
     }
 })
@@ -80,24 +121,6 @@ export const {setIsLoggedInAC} = slice.actions
 
 // thunks
 
-
-export const logoutTC = () => async (dispatch: Dispatch) => {
-    try {
-        dispatch(setAppStatusAC({status: 'loading'}))
-        const data = await authApi.logout()
-        if (data.resultCode === 0) {
-
-            dispatch(setIsLoggedInAC({value: false}))
-            dispatch(setAppStatusAC({status: 'succeeded'}))
-        } else {
-
-            handleServerAppError(data, dispatch)
-        }
-    } catch (e) {
-
-        handleServerNetworkError(e.message, dispatch)
-    }
-}
 
 // types
 
